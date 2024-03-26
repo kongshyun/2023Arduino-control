25a85e7f6eb722c29e64241e035c270917fbead6
kongshyun <tlgus927@naver.com>
2024-03-26 19:29:24 +0900

    0326

--- Firefly_PCB/0326_Linear_photo/0326_Linear_photo.ino
+++ Firefly_PCB/0326_Linear_photo/0326_Linear_photo.ino
@@ -5,6 +5,9 @@
   Quarter step (LHL):1바퀴=800스텝
   Eighth step (HHL):1바퀴 1600스텝
   Sixteenth step (HHH): 1바퀴 3200스텝
+
+
+  리니어 , 모터 1바퀴당 3cm정도 이동
 */
 #include <DMXSerial.h>
 #include <DmxSimple.h>
@@ -21,6 +24,7 @@ const int STEPS_PER_REV = 200 * 32; // 1/32 스텝 모드일 때 1회전 당 스
 const int revNum=4;
 const int inpin=6;//포토센서
 int val;
+int state=1;
 
 void setup() {
   DMXSerial.init(DMXReceiver);
@@ -29,31 +33,27 @@ void setup() {
   pinMode(inpin,INPUT_PULLUP);
   
   if ((digitalRead(inpin))==1){
-    digitalWrite(7,HIGH);delay(100);
-    digitalWrite(7,LOW);delay(100);
-    digitalWrite(7,HIGH);delay(100);
-    digitalWrite(7,LOW);delay(100);
+    digitalWrite(7,HIGH);delay(500);
+    digitalWrite(7,LOW);
   }
   else{
     digitalWrite(7,LOW);
   }
+
 }
 
 
 void loop() {
   unsigned long lastPacket = DMXSerial.noDataSince();
   int signal=DMXSerial.read(30);
-  
-  //대기모드
   if (lastPacket < 5000) {
-    if (signal<100 & signal>50)
-    digitalWrite(7,HIGH);
-    _Stepper_CW_move(32000 ,1000, 1000);//반시계방향회전. 리니어 모터쪽으로 가까이 간다. 말굽센서 값이 바뀌면 함수 아웃
-    _Stepper_CCW_move(-2000 ,400, 400);//모터와 조금 멀어져서 0점 셋팅
-    
-    else if (signal>127){
-      _Stepper_move(STEPS_PER_REV ,3000, 3000);
-    }
+    if(state){
+    _Stepper_CW_move(260000 ,2000, 1000);
+    _Stepper_CCW_move(-6400 ,400, 400);
+    } 
+    if(signal>127){
+      _Stepper_move(200000 ,3000, 1500);
+    }  
   }
   else {
     _Stepper_stop();
@@ -62,9 +62,10 @@ void loop() {
 
 //스텝모터 구동함수(이동거리, 반복횟수)
 void _Stepper_CW_move(long int move , int vel, int acel){
+  digitalWrite(7,LOW);
   myStepper.setMaxSpeed(vel);  // 최대속도 설정 (단위: 스텝/초)
   myStepper.setAcceleration(acel); // 가속도 설정 (단위: 스텝/초^2)
-  myStepper.setSpeed(vel); // 초기 속도 설정 (단위: 스텝/초)
+  myStepper.setSpeed(100); // 초기 속도 설정 (단위: 스텝/초)
   val=digitalRead(inpin);//포토센서의 값을 읽는다
   myStepper.moveTo(move);
   while (val==1){
@@ -72,26 +73,27 @@ void _Stepper_CW_move(long int move , int vel, int acel){
     myStepper.run();
   }
   myStepper.setCurrentPosition(0);
-  digitalWrite(7,HIGH);
-  delay(500);
-  digitalWrite(7,LOW);
-  delay(500);
+  for (int i=0;i<5;i++){
+    digitalWrite(7,HIGH);delay(100);
+    digitalWrite(7,LOW);delay(100);
+  }
+  
 }
 
 void _Stepper_CCW_move(long int move , int vel, int acel){
   myStepper.setMaxSpeed(vel);  // 최대속도 설정 (단위: 스텝/초)
   myStepper.setAcceleration(acel); // 가속도 설정 (단위: 스텝/초^2)
-  myStepper.setSpeed(vel); // 초기 속도 설정 (단위: 스텝/초)
-  //val=digitalRead(inpin);//포토센서의 값을 읽는다
+  myStepper.setSpeed(100); // 초기 속도 설정 (단위: 스텝/초)
   myStepper.moveTo(move);
   while (myStepper.distanceToGo() != 0){
     myStepper.run();  
   }
   myStepper.setCurrentPosition(0);
-  digitalWrite(7,HIGH);
-  delay(500);
-  digitalWrite(7,LOW);
-  delay(500);
+  for (int i=0;i<5;i++){
+    digitalWrite(7,HIGH);delay(300);
+    digitalWrite(7,LOW);delay(300);
+  }
+  state=0;
 }
 
 //스텝모터 구동함수(이동거리, 반복횟수)
@@ -112,10 +114,10 @@ void _Stepper_move(long int move , int vel, int acel){
     myStepper.setCurrentPosition(0);
     i++;
   }
-  digitalWrite(7,HIGH);
-  delay(1000);
-  digitalWrite(7,LOW);
-  delay(1000);
+  for (int i=0;i<5;i++){
+    digitalWrite(7,HIGH);delay(200);
+    digitalWrite(7,LOW);delay(200);
+  }
 }
 
 void _Stepper_stop(){
